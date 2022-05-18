26bacb3b42cb813ef4ab364bd16045f5383ea500
AvishekRoy16 <avishek.roy.india@gmail.com>
2022-05-19 01:07:04 +0530

    Updated Website

--- FRONTEND/src/app/posts/post-detail/post-detail.component.ts
+++ FRONTEND/src/app/posts/post-detail/post-detail.component.ts
@@ -22,7 +22,18 @@ export class PostDetailComponent implements OnInit, OnDestroy {
   userId: String;
   userIsAuthenticated: boolean
   private authStatusSub: Subscription;
+  who: string
   profile: any
+  comments : String[] = ["This is a good blog","Wonderful Thoughts","I found this blog very interesting"," Very cool ","Awesome and cool blog"]
+  Person= ["Adarsh Kumar" , "Keshav Mittal" , "Abhinav Singh", "Abhimanyu Kumar", "Avishek Roy" ]
+  rand : Number[] = []
+  comment: String = '';
+  new_com: String = '';
+  
+
+    myperson:any = this.Person[Math.floor(Math.random() * this.Person.length)]
+
+    // console: any.log(myperson)//This gives you any string from groceries
 
   constructor(
     public postsService: PostService,
@@ -31,6 +42,43 @@ export class PostDetailComponent implements OnInit, OnDestroy {
     private authService: AuthService,
     public profileService:ProfileService
   ) { }
+
+  commenting(){
+    // document.getElementById("comment")= "";
+    this.new_com = this.comment
+    this.who = "By Keshav Mittal"
+    const comm = document.getElementById('avishek')
+    // comm.comment = "";
+
+    // this.Person.push(this.new_com.toString())
+  }
+
+
+  showcc(){
+
+    var x = document.getElementById("mydiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  myper(){
+      return Math.floor(Math.random() * this.Person.length)
+  }
+
+  myfn(){
+    var a = Math.floor(Math.random() * this.Person.length);
+    if(this.rand.includes(a) == false){
+        this.rand.push(a)
+        return a
+        
+    }
+    if(this.rand.includes(a) == true){
+      return this.myfn()
+    }
+  }
 
   ngOnInit(): void {
     this.url = this.router.url.split("/")[1]
@@ -108,6 +156,30 @@ export class PostDetailComponent implements OnInit, OnDestroy {
     })
 
   }
+
+  getcommentById(id) {
+    this.isloading = true
+    this.postsService.getPost(this.postId).subscribe(postData => {
+      console.log(postData)
+      this.post = {
+        id: postData._id,
+        title: postData.title,
+        content: postData.content,
+        imagePath: postData.imagePath,
+        creator: postData.creator,
+        postDate:postData.postDate
+      };
+      this.getPostUserByCreatorId(postData.creator) 
+
+      // this.compareIds(this.userId,this.post.creator)
+      this.isloading = false
+    })
+    e => {
+      this.isloading = false
+      this.error = e
+    }
+  }
+
   ngOnDestroy() {
 
     this.authStatusSub.unsubscribe();
